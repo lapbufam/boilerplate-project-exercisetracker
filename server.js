19e19770ecd6e7120ed146c71e42b127eae6cf39
Leonardo Barreto <lapb@icomp.ufam.edu.br>
2021-04-02 16:02:53 -0400

    fix: add nodemon, uuid and delete unused files

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+let users = [];
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/exercise/users", (req, res) => {
+  res.json(users);
+});
 
+app.post("/api/exercise/new-user", (req, res) => {
+  console.log(req.body);
+  const newUser = {
+    _id: uuidv4(),
+    username: req.body.username,
+  };
 
+  users.push(newUser);
 
+  res.send(newUser);
+});
+
+app.post("/api/exercise/add", (req, res) => {
+  console.log("add exercise");
+});
+
+app.get("/api/exercise/log?{userId}[&from][&to][&limit]", (req, res) => {
+  console.log("get logs");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
